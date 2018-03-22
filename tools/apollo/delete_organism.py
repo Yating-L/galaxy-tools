a4e2ef5df02356b783e786e2fdc6565ee6d87be2
Yating Liu <yatinghust@gmail.com>
2018-03-22 16:56:08 -0500

    check galaxy user permission: delete the organism if the gx_user is global admin or organism administrative

--- tools/apollo/delete_organism.py
+++ tools/apollo/delete_organism.py
@@ -3,19 +3,12 @@ from __future__ import print_function
 
 import argparse
 import logging
-import json
+import sys
+
 
 from webapollo import AssertUser, GuessOrg, OrgOrGuess, WAAuth, WebApolloInstance
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-def checkPermission(gx_user, org_cn):
-    org_permissions = wa.users.getOrganismPermissionsForUser(gx_user)
-    for organism in org_permissions:
-        if org_cn == organism['organism'] and "ADMINISTRATE" in organism['permissions']:
-            return True
-    return False
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sample script to completely delete an organism')
@@ -29,20 +22,26 @@ if __name__ == '__main__':
     # User must have an account
     gx_user = AssertUser(wa.users.loadUsers(email=args.email))
 
-    # Get organism
+    # Get organism commonName
     org_cn = GuessOrg(args, wa)
-    if org_cn:
-        # Check user perms on org.
+    if isinstance(org_cn, list):
         org_cn = org_cn[0]
-        org = wa.organisms.findOrganismByCn(org_cn)
-        if checkPermission(gx_user, org_cn):
-            returnData = wa.organisms.deleteOrganism(org['id'])
-            print("Delete organism %s" % org_cn)
-            print("returnData = " + str(returnData) + "\n")
-        else:
-            logger.error("Not authorized! You do not have permissions to delete this organism. You need to request administrative permission for this organism from the owner.")
-            exit(1)
-    else:
-        logger.error("Organism %s doesn't exist" % org_cn)
 
+    org = wa.organisms.findOrganismByCn(org_cn)
+
+    if not org:
+        sys.exit("Organism %s doesn't exist" % org_cn)
+
+    # check if the gx_user is global admin or organism administrative
+    has_perms = False
+    for user_owned_organism in gx_user.organismPermissions:
+        if user_owned_organism['organism'] == org['commonName'] and 'ADMINISTRATE' in user_owned_organism['permissions']:
+            has_perms = True
+            break
+    if not has_perms and gx_user.role != 'ADMIN':
+        sys.exit(gx_user.username + " is not authorized to delete this organism. You need to request administrative permission for this organism from the owner.")
+
+    returnData = wa.organisms.deleteOrganism(org['id'])
+    print("Delete organism %s" % org_cn)
+    print("returnData = " + str(returnData) + "\n")
 
